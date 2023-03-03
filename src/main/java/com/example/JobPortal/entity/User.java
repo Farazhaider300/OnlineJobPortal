5b385dbcbf886d437baa437e1d0ef7b8eb17e872
HP <mfarazhaider300@gmail.com>
2023-03-03 16:19:48 +0500

    Job Portal

---
+++ src/main/java/com/example/JobPortal/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.JobPortal.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private userRole status;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private long password;
+    public enum userRole{
+        JOB_SEEKER,
+        JOB_POSTER
+    }
+
+}
---
+++ src/main/java/com/example/JobPortal/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.JobPortal.repository;
+
+import com.example.JobPortal.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
